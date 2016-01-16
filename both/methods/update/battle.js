7b4fbf5894dcf8097bf0dc78da745b86f177369b
Huy Do <mentosti29071994@gmail.com>
2016-01-16 16:40:43 +0700

    logic

--- both/methods/update/battle.js
+++ both/methods/update/battle.js
@@ -39,7 +39,7 @@ Meteor.methods({
         var documentId = Battle.update(argument.battleId, {
           $set: {
             startTime: Date.now(),
-            battleText: 'After spending two years in the Office for House-Elf Relocation, Scamander joined the Beast Division and put his knowledge of magical beasts to good use. In 1918, Scamander was commissioned by Augustus Worme of Obscurus Books to write Fantastic Beasts and Where to Find Them.'
+            battleText: 'After spending'
           }
         });
         return documentId;
