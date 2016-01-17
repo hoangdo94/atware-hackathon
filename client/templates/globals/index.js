50c5afb60e61a18b16e10755ddf1cf244fa98964
Hoang Do <hoangdo.dh94@gmail.com>
2016-01-17 12:39:13 +0700

    final

--- client/templates/globals/index.js
+++ client/templates/globals/index.js
@@ -1,3 +1,4 @@
 Template.index.helpers({
-  recentBattles: () => Battle.find()
+  recentBattles: () => Battle.find(),
+  rankings: () => GameProfile.find({}, {sort: {'profile': -1}, limit: 10})
 });
